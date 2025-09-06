bf43f649af8871c48b1734766ef62ca4a1eb75e5
Evan Adkins <evanadkins@wecancodeit.com>
2025-09-06 16:25:15 -0400

    seasonal image changes based on theme

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+let isPromoApplied = false;
+
+
+
 const changeTheme = () => {
+    isPromoApplied = !isPromoApplied
     document.body.classList.toggle("sesaonal-promo")
+
+    if(isPromoApplied) {
+        document.querySelector("#seasonal-promo-img").setAttribute("src", "./assets/images/irish.jpg")
+    }
 }
+
+// &&(AND) ||(OR) !(NOT)
